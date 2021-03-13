91899af7d6fcacb55367389c6e31e374af324d47
Riverside Rocks <trent@riverside.rocks>
2021-03-13 15:13:41 -0500

    Add more verbose data

--- data.php
+++ data.php
@@ -4,6 +4,9 @@ header("Content-type: application/json");
 
 require __DIR__ . "/vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $servername = $_ENV['MYSQL_SERVER'];
 $username = $_ENV["MYSQL_USERNAME"];
 $password = $_ENV["MYSQL_PASSWORD"];
@@ -19,9 +22,45 @@ if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) $_SERVER['REMOTE_ADDR'] = $_SERVER
 $website = $_GET["website"];
 
 $sql = "SELECT * FROM a WHERE website=?";
+//$stmt->bind_param("s", $website);
 $stmt = $conn->prepare($sql); 
 $stmt->bind_param("s", $website);
 $stmt->execute();
+//$stmt->close();
+$result = $stmt->get_result();
+$hits = array();
 while ($row = $result->fetch_assoc()) {
-    print_r($row);
+    array_push($hits, $row["country"]);
 }
+
+//$final = array("hits" => count($hits));
+//echo json_encode($final);
+
+
+
+$sql = "SELECT country, count(*) as hits from a GROUP BY country ORDER BY hits DESC";
+$stmt = $conn->prepare($sql); 
+$stmt->execute();
+$result = $stmt->get_result();
+$countries = array();
+$hits = array();
+while ($row = $result->fetch_assoc()) {
+    $country = $row["country"];
+    if(! $country)
+    {
+        $country = "Unknown";
+    }
+    $hit = $row["hits"];
+    array_push($countries, $country);
+    array_push($hits, $hit);
+}
+
+
+echo json_encode(array(
+    "total_hits" => count($hits),
+    "verbose" =>
+        array(
+            "countries" => $countries,
+            "hits" => $hits,
+        )
+));

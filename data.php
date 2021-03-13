39a12ec7a93805b111c205a0f833f2543df2d3ac
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-03-13 14:41:54 -0500

    Data dump

--- data.php
+++ data.php
@@ -16,3 +16,12 @@ if ($conn->connect_error) {
 
 if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 
+$website = $_GET["website"];
+
+$sql = "SELECT * FROM a WHERE website=?";
+$stmt = $conn->prepare($sql); 
+$stmt->bind_param("s", $website);
+$stmt->execute();
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
+}
